021bab8663622818436add01956ba20457c6df93
Fernando Iglesias <nando.freitas.iglesias@gmail.com>
2024-11-13 17:38:54 -0400

    Desafio concluido

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+export const api = axios.create({
+    baseURL: 'https://jjiodmtskrojvqqoixwy.supabase.co/rest/v1/',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqaW9kbXRza3JvanZxcW9peHd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1MjkxMjUsImV4cCI6MjA0NzEwNTEyNX0.rFHRgvVccFEl8k4t7FImggtiy7j5FEiv9uk6VeQixL0",
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqaW9kbXRza3JvanZxcW9peHd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1MjkxMjUsImV4cCI6MjA0NzEwNTEyNX0.rFHRgvVccFEl8k4t7FImggtiy7j5FEiv9uk6VeQixL0"
     }
-})
+});
